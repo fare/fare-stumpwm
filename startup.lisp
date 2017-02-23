5cd76347caa6e7e7bdbc8fa3b2646e7b8f87725b
Francois-Rene Rideau <fare@tunes.org>
2017-02-23 17:17:26 -0500

    Read .xmodmap at startup

--- startup.lisp
+++ startup.lisp
@@ -6,6 +6,8 @@
     (cond
       ((equal hostname "flatland")
        (inferior-shell:run '("xrandr" "--output" "eDP1" "--mode" "3200x1800" "--auto")))))
+  (uiop:if-let (modmap (uiop:probe-file* (uiop:subpathname (user-homedir-pathname) ".Xmodmap")))
+               (run-shell-command "xmodmap ~/.Xmodmap"))
   (activate-terminator)
   (run-shell-command "xscreensaver -nosplash")
   (run-commands "grename 1" "gnewbg 2"))
