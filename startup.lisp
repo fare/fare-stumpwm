d9c3e19128f73315fc27e0994f2992b97ad5181f
Francois-Rene Rideau <fare@tunes.org>
2023-09-14 11:14:42 -0400

    Tweak startup

--- startup.lisp
+++ startup.lisp
@@ -4,6 +4,14 @@
   #+sbcl (sb-unix:uid-username (sb-unix:unix-getuid))
   #-sbcl (last (pathname-directory (user-homedir-pathname)))) ;; bad approximation?
 
+(defun is-guix-p ()
+  (and (uiop:probe-file* "/run/current-system/")
+       (not (uiop:probe-file* "/etc/NIXOS"))))
+(defun is-nixos-p ()
+  (and (uiop:probe-file* "/run/current-system/")
+       (uiop:probe-file* "/etc/NIXOS")
+       t))
+
 ;;; Start up
 (defun my-start ()
   (let ((hostname (uiop:hostname)))
@@ -17,8 +25,6 @@
              hostname
              (logname)
              (uiop:subpathname (user-homedir-pathname) "etc/X11/Xdefaults.m4")))
-    ;; Looks like GUIX obsoleted xscreensaver with its own system
-    (run-shell-command "[ -f /etc/NIXOS ] && xscreensaver -nosplash")
     (activate-initial-clients)
     ;;(run-commands "grename 1" "gnewbg 2")
     nil))
@@ -27,9 +33,11 @@
 
 (defun activate-initial-clients ()
   (unless (or *initial-clients-activated* (all-windows))
+    (setf *initial-clients-activated* t)
+    (unless (is-guix-p) ;; Looks like GUIX obsoleted xscreensaver with its own system
+      (run-shell-command "xscreensaver -nosplash"))
     (activate-emacs)
     (activate-terminator)
-    (activate-chromium))
-  (setf *initial-clients-activated* t))
+    (activate-chromium)
 
 (register-stumpwm-start-hook 'my-start)
